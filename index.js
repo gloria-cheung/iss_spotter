ccb21510a7723c11d422157d7b5889f6db8251e9
Gloria <gloriacheung812@gmail.com>
2022-04-21 22:39:14 +0000

    test manually for fetchISSFlyOverTimes func

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP} = require("./iss");
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes} = require("./iss");
 
 // // Manually testing code:
 // fetchMyIP((error, ip) => {
@@ -18,4 +18,14 @@ const { fetchMyIP, fetchCoordsByIP} = require("./iss");
 //   }
 
 //   console.log("It worked! Returned coords:" , coords)
+// })
+
+// // Manually testing code:
+// fetchISSFlyOverTimes({ latitude: 43.6547, longitude: -79.3623 }, (error, arr ) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Returned array of ISS fly over times:" , arr)
 // })
