2634118b40787cc3e664ca624e680c6b4f5cf2a7
Gloria <gloriacheung812@gmail.com>
2022-04-21 18:58:27 +0000

    test iss.js function and require module

---
+++ index.js
@@ -0,0 +1,11 @@
+const { fetchMyIP } = require("./iss");
+
+// // Manually testing code:
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
