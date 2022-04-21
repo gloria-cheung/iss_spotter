f36cd935d83ed6245f935f91d481614650fdedb1
Gloria <gloriacheung812@gmail.com>
2022-04-21 23:04:08 +0000

    eslint file

--- iss.js
+++ iss.js
@@ -85,12 +85,12 @@ const fetchISSFlyOverTimes = function(coords, callback) {
 /**
  * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
  * Input:
- *   - A callback with an error or results. 
+ *   - A callback with an error or results.
  * Returns (via Callback):
  *   - An error, if any (nullable)
  *   - The fly-over times as an array (null if error):
  *     [ { risetime: <number>, duration: <number> }, ... ]
- */ 
+ */
 const nextISSTimesForMyLocation = function(callback) {
   fetchMyIP((error, ip) => {
     if (error) {
@@ -104,10 +104,10 @@ const nextISSTimesForMyLocation = function(callback) {
         if (error) {
           return callback(error, null);
         }
-        callback(error, arr)
-      })
-    })
-  })
+        callback(error, arr);
+      });
+    });
+  });
 };
 
 module.exports = { nextISSTimesForMyLocation };
